172b3e774daecbf156acad035cc13a7ca19ed0c6
Andrey <teryaev.2004@mail.ru>
2023-03-04 22:23:39 +0300

    realization-login-form-functional

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,23 +1,54 @@
 import React, { FC } from "react";
-import {Link} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
 
 const LoginPage: FC = () => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const changeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const formHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await axios
+      .post(process.env.REACT_APP_PROXY + "/api/auth/login", {
+        userName,
+        password,
+      })
+      .then((res: AxiosResponse) => {
+        alert(res.data.message);
+
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+
   return (
     <div className="login-box">
       <h2>Авторизация</h2>
       <form>
         <div className="user-box">
-          <input type="text"/>
+          <input type="text" onChange={changeUserName} value={userName} />
           <label>Имя пользователя</label>
         </div>
         <div className="user-box">
-          <input type="password" />
+          <input type="password" onChange={changePassword} value={password} />
           <label>Пароль</label>
         </div>
         <div>
-        <Link to="/auth/reg">Зарегистрироваться</Link>
+          <Link to="/auth/reg">Зарегистрироваться</Link>
         </div>
-        <button>
+        <button onClick={formHandler}>
           <span></span>
           <span></span>
           <span></span>
