15fcfd5873df5b5f39f1d6dabada96bb6acb5463
Andrey <teryaev.2004@mail.ru>
2023-03-05 00:40:40 +0300

    add-delete-btn-for-save-component

--- src/components/Saves/Save/Save.tsx
+++ src/components/Saves/Save/Save.tsx
@@ -5,7 +5,10 @@ const Save: FC = () => {
     return (
         <div className={styles.wrapper}>
             <p>Название</p>
-            <div className={styles.importBtn}>Импорт</div>
+            <div className={styles.wrapperBtns}>
+                <div className={styles.importBtn}>Импорт</div>
+                <div className={styles.deleteBtn}>Удалить</div>
+            </div>
         </div>
     );
 };
